b14efa266f1ea36daa64f7213c24683ff3287910
you7n <qileniao@yeah.net>
2025-07-02 21:42:41 +0800

    change api key

--- pragmatic_figurative_language_classifier.py
+++ pragmatic_figurative_language_classifier.py
@@ -2,7 +2,7 @@ import json
 import openai
 import os
 
-openai_api_key = os.environ.get("OPENAI_API_KEY")
+openai_api_key = os.environ.get("OPENAI_API_KEY") 
 openai.api_base = "https://aicvw.com/v1"
 client = openai.OpenAI(base_url="https://aicvw.com/v1",api_key=openai_api_key)
 
